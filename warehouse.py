6de00518c6f1b7571918b493be7522ef8f51788f
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-09 14:39:30 +0100

    Update objectives and tasks

---
+++ warehouse.py
@@ -0,0 +1,5 @@
+# Program a system to manage inventory for a small store or warehouse. Track items, quantities, and categories.
+# Practice Skills: Use dictionaries to store item details and quantities. 
+# Lists can be used to store categories or item names. 
+# Practice updating inventory (adding, removing items), and retrieving items based on certain criteria like category or quantity range. 
+# String methods can be used for input validation and formatting.
