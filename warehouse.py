90d2a7620c087a291322d605855322071af81c51
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-10 14:14:40 +0100

    Update warehouse.py

--- warehouse.py
+++ warehouse.py
@@ -6,20 +6,39 @@
 # Using sets: checking for duplicates and easy access if a particular item is in stock. 
 
 # building a dictionary structured in dict: categories subdict: Items : Quantities
-inventory = {"tools":"", "cutlery":"", "towels":"", "kitchenware":"",}
-keys = ["tools", "cutlery", "towels", "kitchenware"]
+from prettytable import PrettyTable
+
+inventory = {"tools":{}, "cutlery":{}, "towels":{}, "kitchenware":{},}
 items = ["cutlery", "knife", "tools", "hammer","kitchenware", "toaster", "towels", "cotton towel","towels", "satin towel", "tools", "screwdriver", "cutlery", "fork", "cutlery", "spoon", "cutlery", "teaspoon"]
 quantities = [2, 4, 1, 5, 2, 8, 5, 3, 2]
 
+def type_item_quantity():
+    return list(zip(items[0::2], items[1::2], quantities))
+
+def entries_dictionary(overview):
+    for category, item, quantity in overview:
+        inventory[category][item] = quantity
+    return inventory
+
+table_inventory = PrettyTable(entries_dictionary(type_item_quantity()))
+
+print(table_inventory)
+
+# def import_to_inventory():
+#     for items in type_item_quantity():
+#         if inventory[keys] == keys:
+#             inventory.values == values
+#         print(inventory)
+
+# import_to_inventory()
 
 
-# def type_item_quantity():
-#     return list(zip(items[0::2], tuple(zip(items[1::2], quantities))))
-
+# import_to_inventory()
+# print(inventory)
 # for i, d in type_item_quantity():
-#     inventory[i] = d
+#     if inventory[] == i:
+#         inventory[i] = d
 
-print(inventory)
 
 # def create_entries():
 #     inventory = dict(zip(type_item_quantity[0::2] type_item_quantity()))
