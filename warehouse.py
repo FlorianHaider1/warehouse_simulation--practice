586e21ccaa2de3e3662019e919b9056a31156ebb
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-10 08:44:36 +0100

    Update warehouse.py

--- warehouse.py
+++ warehouse.py
@@ -3,3 +3,4 @@
 # Lists can be used to store categories or item names. 
 # Practice updating inventory (adding, removing items), and retrieving items based on certain criteria like category or quantity range. 
 # String methods can be used for input validation and formatting.
+# Using sets: checking for duplicates and easy access if a particular item is in stock. 
